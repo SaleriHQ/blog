16f7914636bfebb42cd109a58e851d89f6195a8c
SaleriHQ <1665800094@qq.com>
2025-06-01 19:05:24 +0800

    feat: font

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,35 @@
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import rehypePrettyCode from 'rehype-pretty-code';
 import vue from '@astrojs/vue';
+import theme from "./public/theme/theme.json"
 import tailwindcss from "@tailwindcss/vite";
+import { transformerCopyButton } from '@rehype-pretty/transformers';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'http://saleri.xyz',
     integrations: [mdx(), sitemap(), vue()],
+
+    markdown: {
+        syntaxHighlight: false,
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [
+            rehypeKatex,
+            [rehypePrettyCode, {
+                theme: theme,
+                transformers: [
+                    transformerCopyButton({
+                        visibility: 'hover',
+                        feedbackDuration: 2_500,
+                    }),
+                ],
+            }]
+        ]
+    },
 
     vite: {
         plugins: [tailwindcss()]
---
+++ src/content/blog/MIT.18.01.Differentiation.md
@@ -0,0 +1,29 @@
+---
+title: "Test"
+description: "Test"
+pubDate: "2025.6.1"
+updatedDate: "2025.6.1"
+tags: []
+---
+
+# H1
+## H2
+### H3
+#### H4
+##### H5
+###### H6
+
+```c++
+int main() {
+    printf("Hello World");
+    return 0;
+}
+```
+
+$$
+\lim_{\Delta x \to 0}{\frac{\Delta f}{\Delta x}}= \frac{f(x_0 + \Delta x) - f(x_0)}{\Delta x} = f'(x_0)
+$$
+
+> Block  
+> 
+> Block
